761cd355e2e8032ea29c21507f7a5cd3b6e79d41
a.pirogov <a.pirogov@fz-juelich.de>
2023-06-15 11:49:23 +0200

    add dummy conftest.py, closes #9

---
+++ {{ cookiecutter.__project_slug }}/tests/conftest.py
@@ -0,0 +1,9 @@
+"""Global pytest configuration."""
+
+# NOTE: you can put your pytest fixtures here.
+#
+# Fixtures are very useful for automating repetitive test preparations
+# (such as accessing resources or creating test input files)
+# which are needed in multiple similar tests.
+#
+# see: https://docs.pytest.org/en/6.2.x/fixture.html
